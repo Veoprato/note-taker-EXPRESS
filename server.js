92a8dabeeab751d0a7df55e1356d048ac5f12698
Veoprato <75872616+Veoprato@users.noreply.github.com>
2022-01-16 06:53:36 -0500

    htmlRoutes hooked up to server.js

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
+const htmlRoutes = require('./routes/htmlRoutes')
+
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+app.use(express.static('public'));
+
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is live! Port ${PORT}`);
