57cf23cae01c4bc556d5ca96b34b460025129ae2
Veoprato <75872616+Veoprato@users.noreply.github.com>
2022-01-16 09:29:06 -0500

    Added use for apiRoutes

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const htmlRoutes = require('./routes/htmlRoutes')
+const apiRoutes = require('./routes/apiRoutes')
 
 const express = require('express');
 const PORT = process.env.PORT || 3001;
@@ -6,6 +7,12 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(express.static('public'));
 
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
+app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
